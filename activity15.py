46aef6ef05374661852cb433f72cf7933d3acdfb
sivaganeshbsg-eng <sivaganesh.bsg@gmail.com>
2025-08-16 17:43:00 +0530

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+    print("you  eligible for driving license.")
+else:
+    print("You are not eligible to vote.")
+    print("you are not eligible for driving license.")
